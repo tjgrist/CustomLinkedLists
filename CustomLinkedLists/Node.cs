2525b2bfbaf6c19d26f6446e244b86dd11bf7212
tjgrst <tjgrst@gmail.com>
2016-05-19 10:03:44 -0500

    initial commit

---
+++ CustomLinkedLists/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLinkedLists
+{
+    class Node
+    {
+        public char myCharacter;
+        public Node nextNode;
+        public Node(char character)
+        {
+            myCharacter = character;
+        }
+    }
+}
---
+++ CustomLinkedLists/SystemString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLinkedLists
+{
+    class SystemString : ICustomString
+    {
+        string myString;
+
+        public SystemString(string name)
+        {
+            myString = name;
+        }
+        public override string ToString()
+        {
+            return myString;
+        }
+        public void Insert(int startIndex, string stringToInsert)
+        {
+            myString = myString.Insert(startIndex, stringToInsert);
+        }
+
+        public void Remove(int startIndex, int numCharsToRemove)
+        {
+            myString = myString.Remove(startIndex, numCharsToRemove);
+        }
+
+        public int Length()
+        {
+            return myString.Length;
+        }
+    }
+}
